0aedfe65d0d00c5d8d20ed8ef0a2d3f2a58af73d
Batuhan İnce <inceb97@gmail.com>
2023-01-22 02:39:59 +0300

    prettier

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import { useEffect, useState } from "react";
 import useGeolocation from "react-hook-geolocation";
 import Forecast from "./components/weather";
 
-
 const App = () => {
   const [weather, setWeather] = useState();
   const { latitude, longitude } = useGeolocation();
-  
+
   const getWeatherData = async (lat, lon) => {
     const key = process.env.REACT_APP_WEATHER_API_KEY;
-    
+
     try {
       const { data } = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=metric`
-        );
-        setWeather(data);
-      } catch {
+      );
+      setWeather(data);
+    } catch {
       alert("Could not get the data.");
     }
   };
   useEffect(() => {
     latitude && longitude && getWeatherData(latitude, longitude);
-  }, [latitude, longitude])
-  
-  return <div>
-    <Forecast weather={weather} />
-  </div>
+  }, [latitude, longitude]);
+
+  return (
+    <div>
+      <Forecast weather={weather} />
+    </div>
+  );
 };
 
 export default App;
--- src/components/weather.js
+++ src/components/weather.js
@@ -1,47 +1,65 @@
 const Forecast = (props) => {
-    const { weather } = props;
-    const options = {
-        weekday: "short",
-        year: "numeric",
-        month: "short",
-        day: "numeric",
-    };
-    
-    const descriptionIcons = {
-        "clear sky": "01d.png",
-        "few clouds": "02d.png",
-        "scattered clouds": "03d.png",
-        "broken clouds": "04d.png",
-        "shower rain": "09d.png",
-        "rain": "10d.png",
-        "thunderstorm": "11d.png",
-        "snow": "13d.png",
-        "mist": "50d.png",
-    };
-    
-    if (!weather) {
-        return <p id="loading">Loading...</p>
-    }
-    
-    
-    const background = {
-        "TR": "Turkey",
-        "DE": "Germany"
-    };
-    
-    let air_icon = `http://openweathermap.org/img/wn/${descriptionIcons[weather.weather[0].description]}`
+  const { weather } = props;
+  const options = {
+    weekday: "short",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  };
 
-    return <div id={background[weather.sys.country]}>
-        <section id="weatherInfo">
-            <h3>{weather.name}</h3>
-            <p>{Math.round(weather.main.temp)}°C (Feels Like: {Math.round(weather.main.feels_like)}°C)</p>
-            <div>
-                <h4 id="weatherDescription">{weather.weather.map(data => data.description).join(", ")} <img id="air_icon" src={air_icon} alt="air_icon" /></h4> 
-            </div>
-            <p>{new Date(weather.dt * 1000).toLocaleDateString("en-US", options)}</p>
-        </section>
+  const descriptionIcons = {
+    "clear sky": "01d.png",
+    "few clouds": "02d.png",
+    "scattered clouds": "03d.png",
+    "broken clouds": "04d.png",
+    "shower rain": "09d.png",
+    rain: "10d.png",
+    thunderstorm: "11d.png",
+    snow: "13d.png",
+    mist: "50d.png",
+  };
 
-    </div>;
-}
+  if (!weather) {
+    return <p id="loading">Loading...</p>;
+  }
 
-export default Forecast
+  const randomBackground = (arr) => {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
+  const list = [1,2,3,4,5];
+  console.log(randomBackground(list))
+
+
+  const background = {
+    TR: "Turkey",
+    DE: "Germany",
+  };
+
+  let air_icon = `http://openweathermap.org/img/wn/${
+    descriptionIcons[weather.weather[0].description]
+  }`;
+  return (
+    <div id={background[weather.sys.country]}>
+      <section id="weatherInfo" >
+        
+        <h3>{weather.name}</h3>
+        <p>
+          {Math.round(weather.main.temp)}°C (Feels Like:{" "}
+          {Math.round(weather.main.feels_like)}°C)
+        </p>
+        <div>
+          <h4 id="weatherDescription">
+            {weather.weather.map((data) => data.description).join(", ")}{" "}
+            <img id="air_icon" src={air_icon} alt="air_icon" />
+          </h4>
+        </div>
+        <p>
+          {new Date(weather.dt * 1000).toLocaleDateString("en-US", options)}
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Forecast;
